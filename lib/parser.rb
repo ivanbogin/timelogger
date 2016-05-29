d3d6401977e412fcb134a90bb2bf57885de5cf45
Ivan Bogin <ivanbogin@gmail.com>
2016-05-29 23:10:26 +0200

    Remove merge text from commit message.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,7 @@
 class Parser
   def normalize_git_message(message)
-    message.tr("\n\r", "")
+    message.tr!("\n\r", "")
+    message.sub!(/merge.*into/i, '')
+    message.strip
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,9 +1,15 @@
 require 'parser'
 
 RSpec.describe Parser, '#normalize_git_message' do
-  it 'removes spaces from commit message' do
+  it 'removes new lines from commit message' do
     parser = Parser.new
     result = parser.normalize_git_message("Same shit, \nbetter smell, \r\nonly 1 query,\r\n\neasy to count.")
     expect(result).to eq "Same shit, better smell, only 1 query,easy to count."
+  end
+
+  it 'removes merge text from message' do
+    parser = Parser.new
+    result = parser.normalize_git_message("Merge branch 'master' of github.com:SEOshop/API into CAM-1570-improve-webhooks.\r\nImportant text.")
+    expect(result).to eq "CAM-1570-improve-webhooks.Important text."
   end
 end
